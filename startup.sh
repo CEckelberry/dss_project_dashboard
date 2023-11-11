591d4df72116ef3eed90cfb634417172a05febc8
Cole Eckelberry <cole.eckelberry@gmail.com>
2023-11-11 13:08:38 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,6 +7,41 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
+# Install Google Chrome
+install_google_chrome() {
+    echo "Installing Google Chrome..."
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    sudo apt-get -f install -y
+    rm google-chrome-stable_current_amd64.deb
+}
+
+# Install ChromeDriver matching Google Chrome version
+install_chromedriver() {
+    # Get the installed version of Google Chrome
+    CHROME_VERSION=$(google-chrome --version | grep -oP "Google Chrome \K[^ ]+")
+    CHROME_MAIN_VERSION=$(echo $CHROME_VERSION | cut -d'.' -f1)
+
+    # Get the corresponding ChromeDriver version
+    CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_MAIN_VERSION")
+
+    echo "Installing ChromeDriver version $CHROMEDRIVER_VERSION for Google Chrome version $CHROME_VERSION..."
+
+    # Download ChromeDriver
+    curl -L "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip" -o chromedriver_linux64.zip
+    
+    # Extract ChromeDriver and move it to a known directory
+    CHROMEDRIVER_DIR="$HOME/.local/bin"
+    unzip -o chromedriver_linux64.zip -d "$CHROMEDRIVER_DIR"
+    rm chromedriver_linux64.zip
+
+    # Add execute permissions
+    chmod +x "$CHROMEDRIVER_DIR/chromedriver"
+
+    # Add the ChromeDriver directory to PATH
+    export PATH="$CHROMEDRIVER_DIR:$PATH"
+}
+
 # Check if Python is installed
 if ! command_exists python3.10; then
     echo "Python 3.10 is not installed. Installing Python 3.10.1."
@@ -45,26 +80,21 @@ if ! command_exists unzip; then
     sudo apt-get install unzip
 fi
 
-# Check if ChromeDriver is installed
+# Install Google Chrome if not installed
+if ! command_exists google-chrome; then
+    install_google_chrome
+fi
+
+# Check if ChromeDriver is installed or if its version matches Google Chrome
 if ! command_exists chromedriver; then
-    echo "ChromeDriver is not installed. Installing ChromeDriver."
-
-    # Determine the most recent version of ChromeDriver
-    CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")
-    
-    # Download ChromeDriver
-    curl -L "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip" -o chromedriver_linux64.zip
-    
-    # Extract ChromeDriver and move it to a known directory
-    CHROMEDRIVER_DIR="$HOME/.local/bin"
-    unzip chromedriver_linux64.zip -d "$CHROMEDRIVER_DIR"
-    rm chromedriver_linux64.zip
-
-    # Add execute permissions
-    chmod +x "$CHROMEDRIVER_DIR/chromedriver"
-
-    # Add the ChromeDriver directory to PATH
-    export PATH="$CHROMEDRIVER_DIR:$PATH"
+    install_chromedriver
+else
+    # Check if the ChromeDriver version matches the Google Chrome version
+    INSTALLED_CHROMEDRIVER_VERSION=$(chromedriver --version | grep -oP "ChromeDriver \K[^ ]+" | cut -d'.' -f1)
+    if [ "$INSTALLED_CHROMEDRIVER_VERSION" != "$CHROME_MAIN_VERSION" ]; then
+        echo "ChromeDriver version does not match Google Chrome version. Reinstalling ChromeDriver..."
+        install_chromedriver
+    fi
 fi
 
 # Define script relative paths
