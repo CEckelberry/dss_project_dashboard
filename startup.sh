b500f86b08ef8b5e1344fb0e49d26beafc8a6553
Cole Eckelberry <cole.eckelberry@gmail.com>
2023-11-11 12:53:20 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -39,6 +39,28 @@ for package in "${required_packages[@]}"; do
     pip3.10 install "$package"
 done
 
+# Check if ChromeDriver is installed
+if ! command_exists chromedriver; then
+    echo "ChromeDriver is not installed. Installing ChromeDriver."
+
+    # Determine the most recent version of ChromeDriver
+    CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")
+    
+    # Download ChromeDriver
+    curl -L "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip" -o chromedriver_linux64.zip
+    
+    # Extract ChromeDriver and move it to a known directory
+    CHROMEDRIVER_DIR="$HOME/.local/bin"
+    unzip chromedriver_linux64.zip -d "$CHROMEDRIVER_DIR"
+    rm chromedriver_linux64.zip
+
+    # Add execute permissions
+    chmod +x "$CHROMEDRIVER_DIR/chromedriver"
+
+    # Add the ChromeDriver directory to PATH
+    export PATH="$CHROMEDRIVER_DIR:$PATH"
+fi
+
 # Define script relative paths
 script_paths=(
     "./downloader_iea/main.py"
